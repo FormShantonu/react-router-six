6bf0bba9035034245a04ed099e00ca3c054a1f1d
Shantonu Chowdhury <chowdhury.shan93@gmail.com>
2021-11-18 23:33:03 +0530

    upto NavLink use

--- react-router-six/src/index.js
+++ react-router-six/src/index.js
@@ -7,8 +7,10 @@ import {
   Routes,
   Route,
   Navigate,
-  Link,
-  Outlet
+  Link, 
+  Outlet,
+  useParams,
+  NavLink
 } from "react-router-dom";
 
 ReactDOM.render(
@@ -18,7 +20,9 @@ ReactDOM.render(
       // In hear we want to a route where hit and redirect to other route
       <Route path="/myClasses" element={<Navigate replace to="/learn" />} />
       <Route path="/learn" element={<Learn />}>
-        <Route path="courses" element={<Courses/>}/>
+        <Route path="courses" element={<Courses/>}>
+          <Route path=":courseId" element={<CourseDetail/>}/>
+        </Route>
         <Route path="bundles" element={<Bundles/>}/>
       </Route>
     </Routes>
@@ -48,10 +52,22 @@ function Learn() {
 }
 
 function Courses() {
+  const coursesList = ["React", "Vue", "Angular", "Node"]
+  const randomName = coursesList[Math.floor(Math.random()*coursesList.length)]
+  console.log(randomName);
   return (
     <div>
       <h1>Course List</h1>
       <h4>Course Items</h4>
+      <NavLink
+      style = {({isActive})=>{
+        return {
+          backgroundColor:isActive?"palegreen":"yellowgreen"
+        }
+      }}
+       to={`/learn/courses/${randomName}`}>{randomName}</NavLink>
+      <NavLink className="btn btn-light" to={`/learn/courses/test`}>test</NavLink>
+      <Outlet/>
     </div>
   );
 }
@@ -61,6 +77,15 @@ function Bundles() {
     <div>
       <h1>Bundles List</h1>
       <h4>Bundles Items</h4>
+    </div>
+  );
+}
+
+function CourseDetail() {
+  const {courseId}=useParams()
+  return (
+    <div>      
+      <h4>This is your url param: {courseId}</h4>
     </div>
   );
 }
