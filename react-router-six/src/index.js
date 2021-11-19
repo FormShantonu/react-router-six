25207f484dac383456a7eb624873136192d3321e
Shantonu Chowdhury <chowdhury.shan93@gmail.com>
2021-11-19 07:47:56 +0530

    end the router v6

--- react-router-six/src/index.js
+++ react-router-six/src/index.js
@@ -7,10 +7,12 @@ import {
   Routes,
   Route,
   Navigate,
-  Link, 
+  Link,
   Outlet,
   useParams,
-  NavLink
+  NavLink,
+  useNavigate,
+  useLocation,
 } from "react-router-dom";
 
 ReactDOM.render(
@@ -20,11 +22,12 @@ ReactDOM.render(
       // In hear we want to a route where hit and redirect to other route
       <Route path="/myClasses" element={<Navigate replace to="/learn" />} />
       <Route path="/learn" element={<Learn />}>
-        <Route path="courses" element={<Courses/>}>
-          <Route path=":courseId" element={<CourseDetail/>}/>
+        <Route path="courses" element={<Courses />}>
+          <Route path=":courseId" element={<CourseDetail />} />
         </Route>
-        <Route path="bundles" element={<Bundles/>}/>
+        <Route path="bundles" element={<Bundles />} />
       </Route>
+      <Route path="/dashboard" element={<CourseDashboard />} />
     </Routes>
   </Router>,
   document.getElementById("root")
@@ -43,31 +46,41 @@ function Learn() {
     <div>
       <h1>Learn</h1>
       <h4>All Courses are Listed here</h4>
-      <Link className="btn btn-success" to="/learn/courses">Courses</Link>
-      {" "}
-      <Link className="btn btn-primary" to="/learn/bundles">Bundles</Link>
-      <Outlet/>
+      <Link className="btn btn-success" to="/learn/courses">
+        Courses
+      </Link>{" "}
+      <Link className="btn btn-primary" to="/learn/bundles">
+        Bundles
+      </Link>
+      <Outlet />
     </div>
   );
 }
 
 function Courses() {
-  const coursesList = ["React", "Vue", "Angular", "Node"]
-  const randomName = coursesList[Math.floor(Math.random()*coursesList.length)]
-  console.log(randomName);
+  // TODO: Create multi dimension array
+  const coursesList = ["React", "Vue", "Angular", "Node"];
+  const randomName =
+    coursesList[Math.floor(Math.random() * coursesList.length)];
+    // send the price by the path
   return (
     <div>
       <h1>Course List</h1>
       <h4>Course Items</h4>
       <NavLink
-      style = {({isActive})=>{
-        return {
-          backgroundColor:isActive?"palegreen":"yellowgreen"
-        }
-      }}
-       to={`/learn/courses/${randomName}`}>{randomName}</NavLink>
-      <NavLink className="btn btn-light" to={`/learn/courses/test`}>test</NavLink>
-      <Outlet/>
+        style={({ isActive }) => {
+          return {
+            backgroundColor: isActive ? "palegreen" : "yellowgreen",
+          };
+        }}
+        to={`/learn/courses/${randomName}`}
+      >
+        {randomName}
+      </NavLink>
+      <NavLink className="btn btn-light" to={`/learn/courses/test`}>
+        test
+      </NavLink>
+      <Outlet />
     </div>
   );
 }
@@ -82,10 +95,26 @@ function Bundles() {
 }
 
 function CourseDetail() {
-  const {courseId}=useParams()
+  const { courseId } = useParams();
+  const navigate = useNavigate();
   return (
-    <div>      
+    <div>
       <h4>This is your url param: {courseId}</h4>
+      <button onClick={() => {
+        navigate("/dashboard",{state:courseId})
+      }} className="btn btn-warning">
+        Price
+      </button>      
+      <Link className="btn btn-light" to="/dashboard" state={"Php"}>Test Link</Link>
+    </div>
+  );
+}
+
+function CourseDashboard() {
+  const location = useLocation()
+  return (
+    <div>
+      <h4>The course {location.state} , prise is -----</h4>
     </div>
   );
 }
